e87913fcdf0e518d95e2789d1998043a76333da7
MichaelRudol <98760082+MichaelRudol@users.noreply.github.com>
2025-03-29 13:23:59 +0100

    Added GPIO

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use crate::rolladenstate::RolladenState;
-
+use rppal::gpio::Gpio;
+use std::process::Command;
 mod rolladenstate;
+mod config;
 
 fn main() {
     println!("Hello, world!");
@@ -8,4 +10,19 @@ fn main() {
     let current_rolladen_state = RolladenState::retrieve_current_state().unwrap();
 
     println!("{:?}", current_rolladen_state.should_be_open);//asdf
+
+    let pin_number = 17;
+
+    // Get access to the GPIO pin
+    let mut pin = Gpio::new().expect("Failed to access GPIO")
+        .get(pin_number)
+        .expect("Failed to get GPIO pin")
+        .into_output();  // Set it as an output pin
+
+    pin.set_high();
+    let output = Command::new("sleep")
+        .arg("5")
+        .output();
+
+    pin.set_low();
 }
--- src/rolladenstate.rs
+++ src/rolladenstate.rs
@@ -3,7 +3,10 @@ use serde_json;
 use std::process::{Command};
 use dirs::home_dir;
 use toml;
-use toml::Value;
+
+
+use crate::config::Config;
+
 
 #[derive(Deserialize, Debug)]
 pub struct RolladenState {
@@ -49,35 +52,4 @@ impl RolladenState {
     }
 }
 
-#[derive(Debug, Deserialize)]
-pub struct Config{
-    /// The address of the API to call
-    #[serde(rename = "API_LOCATION")]
-    pub api_address: String,
 
-    /// The name the API uses to convey the target position of the rolladen
-    #[serde(rename = "ROLLADEN_TARGET_NAME")]
-    pub rolladen_target_name: String,
-
-    /// The name the API uses to communicate the amount of light
-    #[serde(rename = "CURRENT_LIGHT_VALUE_NAME")]
-    pub current_light_value_name: String,
-
-
-
-    #[serde(rename = "DEBUG")]
-    pub debug: Profile,
-    #[serde(rename = "RELEASE")]
-    pub release: Profile,
-}
-
-#[derive(Debug, Deserialize)]
-struct Profile{
-    /// The request delay after a request that didn't change the rolladens state.
-    #[serde(rename = "STANDARD_REQUEST_DELAY")]
-    pub standard_request_delay: String,
-
-    /// The request delay after a request that did change the rolladens state.
-    #[serde(rename = "REQUEST_DELAY_CHANGE")]
-    pub request_delay_change: String,
-}
