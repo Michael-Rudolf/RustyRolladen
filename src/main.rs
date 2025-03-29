a2a0cf6d7b6cc43533c5ee9839a63374865ea295
MichaelRudol <98760082+MichaelRudol@users.noreply.github.com>
2025-03-29 10:13:03 +0100

    API location logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use crate::rolladenstate::RolladenState;
+
+mod rolladenstate;
+
 fn main() {
     println!("Hello, world!");
+
+    _ = RolladenState::retrieve_current_state();
 }
---
+++ src/rolladenstate.rs
@@ -0,0 +1,25 @@
+use serde::{Deserialize};
+use std::process::{Command};
+
+#[derive(Deserialize, Debug)]
+pub struct RolladenState {
+    pub metadata: Metadata,
+}
+
+impl RolladenState {
+    pub fn retrieve_current_state() -> Option<RolladenState> {
+        // Get the API Location
+        let output = Command::new("cat")
+            .arg("~/.config/rolladenstate/rolladenstate.json")
+            .output()
+            .expect("Config file (~/.config/rolladenstate.json) missing.");
+
+        println!("Output: {}", String::from_utf8_lossy(&output.stdout));
+        None
+    }
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Metadata {
+    pub should_be_true: bool
+}
